c283278e8067811b6742623b5cc1f74fd21db458
somename <someemail>
2016-09-23 15:13:40 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -3,7 +3,17 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestBowling {
-
+	
+	@Test
+	public void testBowlingGame_singleframeof2n4() {
+		BowlingGame game = new BowlingGame();
+		Frame frame = new Frame(2, 4);
+		
+		game.addFrame(frame);
+		
+		assertTrue("score of first frame is not 6", game.score() == 6);
+	}
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
